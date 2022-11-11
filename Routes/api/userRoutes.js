965ddf94a6f28a794d4cf10b9fd0f5449024f316
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-11-11 12:38:28 -0600

    readded userRoute code and deleted conteoller because we have a routes folder

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,5 +1,24 @@
-const router = require('express').Router();
-const user = require('../../models/Todo');
-const { Todo, User } = require('../../modes');
+const router = require("express").Router();
+const { Todo, User } = require("../../models");
+
+router.get("/", (req, res) => {
+  res.send("api users");
+});
+
+router.post("/", (req, res) => {
+  res.send("api post user");
+});
+
+router.get("/:id", (req, res) => {
+  res.send("api user ID");
+});
+router.get("/:id/todos", (req, res) => {
+  res.send("api user ID todos");
+  let id = req.params.id;
+});
+
+router.post("/:id/todos", (req, res) => {
+  res.send("api post user ID todos");
+});
 
 module.exports = router;
