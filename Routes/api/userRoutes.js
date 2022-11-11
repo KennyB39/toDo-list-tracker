8767d386f0ffb797c251cb199946a0de3f4517e0
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-11-11 15:27:59 -0600

    working on routes

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,6 +1,39 @@
 const router = require("express").Router();
 const { Todo, User } = require("../../models");
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    // login.location.replace("to-decodeURI.html");
+      //are we using replace right?
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.get("/", (req, res) => {
   res.send("api users");
 });
