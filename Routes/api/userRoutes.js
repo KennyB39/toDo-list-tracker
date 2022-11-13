cde125eafa95adce1816ad07e59913284e06a2ec
Greg Krolick <gregrobk@live.com>
2022-11-13 12:36:27 -0500

    user login

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,26 +1,43 @@
 const router = require('express').Router();
-const { Todo, User } = require('../../models');
+const { User } = require('../../models');
+
+// Create new user
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
-router.get ("/", (req, res) => {
-  res.send ("api users")
-})
+// Route tests..............................................
+// router.get ("/", (req, res) => {
+//   res.send ("api users")
+// })
 
-router.post ("/", (req, res) => {
-  res.send ("api post user")
-})
+// router.post ("/", (req, res) => {
+//   res.send ("api post user")
+// })
 
-router.get ("/:id", (req, res) => {
-  res.send ("api user ID")
-})
-router.get ("/:id/todos",  (req, res) => {
-  res.send ("api user ID todos")
-  let id = req.params.id;
-})
+// router.get ("/:id", (req, res) => {
+//   res.send ("api user ID")
+// })
+// router.get ("/:id/todos",  (req, res) => {
+//   res.send ("api user ID todos")
+//   let id = req.params.id;
+// })
 
-router.post ("/:id/todos", (req, res) => {
-  res.send ("api post user ID todos")
-})
+// router.post ("/:id/todos", (req, res) => {
+//   res.send ("api post user ID todos")
+// })
 
 
 
