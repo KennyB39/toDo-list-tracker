086b96ee9b6dcfb279e8db42f0f6a3f63ad3e679
Greg Krolick <gregrobk@live.com>
2022-11-13 12:54:45 -0500

    add create user

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,6 +1,39 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// User Login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Email or password do not match a record" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Email or password do not match a record" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "Login successful" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
 // Create new user
 router.post("/", async (req, res) => {
   try {
