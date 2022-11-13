5bf7b7babc98f7b141c5c09945710b4dc1118898
Greg Krolick <gregrobk@live.com>
2022-11-13 13:19:17 -0500

    add user creationg

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,11 +1,44 @@
-const router = require('express').Router();
-const { User } = require('../../models');
+const router = require("express").Router();
+const { Todo, User } = require("../../models");
 
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+    // login.location.replace("to-decodeURI.html");
+      //are we using replace right?
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // User Login
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
+    console.log("hello")
     if (!userData) {
       res
         .status(400)
@@ -34,7 +67,8 @@ router.post("/login", async (req, res) => {
 });
 
 
-// Create new user
+
+// create new user
 router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -51,27 +85,25 @@ router.post("/", async (req, res) => {
 });
 
 //
-// Route tests..............................................
-// router.get ("/", (req, res) => {
-//   res.send ("api users")
-// })
+// get route tests-------------------------------- 
+// router.get("/", (req, res) => {
+//   res.send("api users");
+// });
 
-// router.post ("/", (req, res) => {
-//   res.send ("api post user")
-// })
+// router.post("/", (req, res) => {
+//   res.send("api post user");
+// });
 
-// router.get ("/:id", (req, res) => {
-//   res.send ("api user ID")
-// })
-// router.get ("/:id/todos",  (req, res) => {
-//   res.send ("api user ID todos")
+// router.get("/:id", (req, res) => {
+//   res.send("api user ID");
+// });
+// router.get("/:id/todos", (req, res) => {
+//   res.send("api user ID todos");
 //   let id = req.params.id;
-// })
+// });
 
-// router.post ("/:id/todos", (req, res) => {
-//   res.send ("api post user ID todos")
-// })
-
-
+// router.post("/:id/todos", (req, res) => {
+//   res.send("api post user ID todos");
+// });
 
 module.exports = router;
