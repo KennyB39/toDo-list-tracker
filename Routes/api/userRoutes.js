09db8a3c33f2ae3817f8ee9e1b1fe6b6be6af2c8
Greg Krolick <gregrobk@live.com>
2022-11-13 17:10:24 -0500

    add user creation route

--- Routes/api/userRoutes.js
+++ Routes/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { Todo, User } = require("../../models");
+const { User } = require("../../models");
 
+// route login
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -29,6 +30,22 @@ router.post('/login', async (req, res) => {
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// route user creation
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }
