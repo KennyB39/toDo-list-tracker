b4f2b24f40698a7154287b4735fb340a88e73e3a
Greg Krolick <gregrobk@live.com>
2022-11-13 23:08:20 -0500

    add new todo record

--- Routes/api/todoRoutes.js
+++ Routes/api/todoRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
-const { Todo, User } = require('../../models');
+const { Todo } = require('../../models');
 
+// add new todo record
+router.post('/', async (req, res) => {
+  try {
+    const newTodo = await Todo.create(req.body);
+    res.status(200).json(newTodo);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
 module.exports = router;
