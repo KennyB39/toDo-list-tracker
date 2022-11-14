d3292a495dd1849e43be5778a79fc3faeb038eab
Kendrick <kenny@Junias-MacBook-Pro.local>
2022-11-14 13:55:39 -0800

    server.js

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+router.get('/', async (req, res) => {
+    try {
+      
+      res.status(200).sendFile(__dirname, "");
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+
 router.use("/api", apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+const path = require("path")
 const express = require("express");
 const routes = require("./Routes");
-const express = require('express');
 const helpers = require('./utils/auth');
 const app = express();
+const sequelize = require("./config/connection");
+
 // add port
-const PORT = process.env.PORT || 3000;
-
-
-app.use(express.json());
-
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get ("/", (req, res) =>{
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+ 
+})
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
