280e5c817f560a2efc5a3e692be8f55d6bac2425
Greg Krolick <gregrobk@live.com>
2022-11-14 20:13:38 -0500

    add retured express and create homeroute

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const routes = require("./Routes");
+const path = require("path");
+const session = require("express-session");
+const helpers = require("./utils/helpers");
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-// add port
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Now listening"));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
