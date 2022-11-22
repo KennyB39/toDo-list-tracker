2c0e3c1354dfc944471c1cc89ebffdc8988809c6
Greg Krolick <gregrobk@live.com>
2022-11-21 22:16:46 -0500

    Moved to handle bars moved signup page to landing page

--- Routes/homeRoutes.js
+++ Routes/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Todo, User } = require("../models");
 const withAuth = require("../utils/auth");
-const path = require("path");
+//const path = require("path");
 
 
 //-------------PreHandle bars------------------------------
@@ -18,21 +18,21 @@ const path = require("path");
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all todos
     const todoData = await Todo.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['email'],
         },
       ],
     });
 
     // Serialize data so the template can read it
-    const todos = todoData.map((todo) => todo.get({ plain: true }));
+    const todo = todoData.map((todo) => todo.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('todo', { 
       todo, 
       logged_in: req.session.logged_in 
     });
@@ -40,6 +40,29 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/todo/:id', async (req, res) => {
+  try {
+    const todoData = await Todo.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['email'],
+        },
+      ],
+    });
+
+    const todo = todoData.get({ plain: true });
+
+    res.render('todo', {
+      ...todo,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 // ----------------Pre-HandleBARS---------------------------
 // router.get('/new-user', async (req, res) => {
@@ -53,10 +76,30 @@ router.get('/', async (req, res) => {
 // });
 // module.exports = router;
 
+// Use withAuth middleware to prevent access to route
+router.get('/todo', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Todo }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('todo', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/todo');
     return;
   }
 
--- Routes/index.js
+++ Routes/index.js
@@ -2,14 +2,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes.js");
 
-router.get("/", async (req, res) => {
-  try {
-    res.status(200).sendFile(__dirname, "");
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+
 module.exports = router;
